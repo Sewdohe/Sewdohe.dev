f39eeb39ab54adc8ebfe020f24749b02b0e2110c
William Melton <sewdohe@protonmail.com>
2026-02-21 12:15:57 -0500

    fixed where some data got out-of-sync from the update

--- src/config.ts
+++ src/config.ts
@@ -193,7 +193,7 @@ export const siteConfig: SiteConfig = {
   // [CONFIG:FAVICON_THEME_ADAPTIVE]
   faviconThemeAdaptive: false, // If true, favicon switches between favicon-dark.png and favicon-light.png based on browser's system theme preference. If false, always uses favicon.png
   // [CONFIG:DEFAULT_OG_IMAGE_ALT]
-  defaultOgImageAlt: "Astro Modular logo.", // Alt text for the default Open Graph image, public/open-graph.png
+  defaultOgImageAlt: "DivNectar", // Alt text for the default Open Graph image, public/open-graph.png
 
   // Global Settings
   // [CONFIG:THEME]
